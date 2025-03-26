7f75905707d1fe9d010aa0c41315c51f99a0ec6e
xxcj <cyk19951112@gmail.com>
2025-03-26 23:58:04 +0800

    dir

--- hydromodel_dl/datasets/data_dict.py
+++ hydromodel_dl/datasets/data_dict.py
@@ -1,16 +1,24 @@
-from hydrodata_tl.settings.datasets_dir import DATASETS_DIR
+from hydrodata_tl.settings.datasets_dir import DATASETS_DIR as DATASETS_DIR_TL
+from hydrodata_camels.settings.datasets_dir import DATASETS_DIR as DATASETS_DIR_CAMELS
 from hydromodel_dl.datasets.data_sets import (
     BaseDataset,
     DplDataset,
 )
 
 datasets_dirction_dict = {
-    "CAMELS_AUS": DATASETS_DIR["CAMELS_AUS"]["EXPORT_DIR"],
-    "CAMELS_DE": DATASETS_DIR["CAMELS_DE"]["EXPORT_DIR"],
-    "CAMELS_FR": DATASETS_DIR["CAMELS_FR"]["EXPORT_DIR"],
-    "CAMELS_SE": DATASETS_DIR["CAMELS_SE"]["EXPORT_DIR"],
-    "CAMELS_US_daymet": DATASETS_DIR["CAMELS_US_daymet"]["EXPORT_DIR"],
-    "CAMELS_US_ouyang": DATASETS_DIR["CAMELS_US_ouyang"]["EXPORT_DIR"],
+    # Transfer learning evaluation datasets
+    "CAMELS_US_ERA5Land": DATASETS_DIR_TL["CAMELS_US_ERA5Land"]["EXPORT_DIR"],
+    "CAMELS_US_HydroATLAS":DATASETS_DIR_TL["CAMELS_US_HydroATLAS"]["EXPORT_DIR"],
+    "CAMELS_US_HydroATLAS_ERA5Land":DATASETS_DIR_TL["CAMELS_US_HydroATLAS_ERA5Land"]["EXPORT_DIR"],
+    "CAMELS_US_HydroATLAS_ERA5Land_MSWEP": DATASETS_DIR_TL["CAMELS_US_HydroATLAS_ERA5Land_MSWEP"]["EXPORT_DIR"],
+    "Changdian_HydroATLAS_ERA5Land":DATASETS_DIR_TL["Changdian_HydroATLAS_ERA5Land"]["EXPORT_DIR"],
+    "Changdian_HydroATLAS_ERA5Land_MSWEP": DATASETS_DIR_TL["Changdian_HydroATLAS_ERA5Land_MSWEP"]["EXPORT_DIR"],
+    # Global CAMELS benchmark datasets
+    "CAMELS_AUS": DATASETS_DIR_CAMELS["CAMELS_AUS"]["EXPORT_DIR"],
+    "CAMELS_DE": DATASETS_DIR_CAMELS["CAMELS_DE"]["EXPORT_DIR"],
+    "CAMELS_FR": DATASETS_DIR_CAMELS["CAMELS_FR"]["EXPORT_DIR"],
+    "CAMELS_SE": DATASETS_DIR_CAMELS["CAMELS_SE"]["EXPORT_DIR"],
+    "CAMELS_US_daymet": DATASETS_DIR_CAMELS["CAMELS_US_daymet"]["EXPORT_DIR"],
 }
 
 
