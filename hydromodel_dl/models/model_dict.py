41e2b7f6c406ea6e836055ff4ba9ae5bd30bfa41
xxcj <cyk19951112@gmail.com>
2025-07-10 09:50:42 +0800

    loss

--- hydromodel_dl/models/model_dict.py
+++ hydromodel_dl/models/model_dict.py
@@ -3,7 +3,6 @@ from hydromodel_dl.models.lstm import SimpleLSTM
 from hydromodel_dl.models.dpl4hbv import DplLstmHbv
 from hydromodel_dl.models.dpl4xaj import DplLstmXaj
 from hydromodel_dl.models.crits import (
-    MultiOutLoss,
     MAELoss,
     MSELoss,
     RMSELoss,    
@@ -20,7 +19,6 @@ pytorch_model_dict = {
 pytorch_opt_dict = {"Adam": Adam}
 
 pytorch_criterion_dict = {
-    "MultiOutLoss": MultiOutLoss,
     "MAE": MAELoss,
     "MSE": MSELoss,
     "RMSE": RMSELoss,
